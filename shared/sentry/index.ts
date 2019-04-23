8bddee2aa4c890c5b26653de7d2cdb5a3444b984
Naina Razafindrabiby <nainarazz@gmail.com>
2019-04-23 19:42:51 +0300

    sentry error tracking backend

--- shared/sentry/index.ts
+++ shared/sentry/index.ts
@@ -2,13 +2,22 @@ import * as Sentry from '@sentry/node';
 import { RewriteFrames } from '@sentry/integrations';
 import { rootDirectory } from '../../api/root';
 
-export default () => {
-	Sentry.init({
-		dsn: 'https://85107c28501a4928af9d6f858492d8d1@sentry.io/1443816',
-		integrations: [
-			new RewriteFrames({
-				root: rootDirectory,
-			}),
-		],
-	});
+let initSentry = () => {
+	// tslint:disable-next-line:no-console
+	console.log('sentry not working in development mode');
 };
+
+if (process.env.NODE_ENV === 'production') {
+	initSentry = () => {
+		Sentry.init({
+			dsn: process.env.SENTRY_DSN,
+			integrations: [
+				new RewriteFrames({
+					root: rootDirectory,
+				}),
+			],
+		});
+	};
+}
+
+export default initSentry;
