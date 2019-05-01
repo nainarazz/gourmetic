7bf004c27eba64fbdaf2f2e7adc831c6e5991ee2
Naina Razafindrabiby <nainarazz@gmail.com>
2019-05-01 11:31:13 +0300

    created header component and added search bar and buttons

--- src/layout/components/header/header.tsx
+++ src/layout/components/header/header.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+	Button,
+	Container,
+	Logo,
+	ButtonsContainer,
+	HeaderItems,
+	SearchBar,
+} from './styles';
 
 export const Header = () => (
-	<div>
-		<Container>Header component</Container>
-	</div>
+	<React.Fragment>
+		<Container>
+			<HeaderItems>
+				<Logo>
+					<span>gourmetic</span>
+				</Logo>
+				<SearchBar>
+					<input
+						type="text"
+						placeholder="What food are you looking for?"
+					/>
+					<button type="submit">
+						<i />
+					</button>
+				</SearchBar>
+				<ButtonsContainer>
+					<Button>Meal Planner</Button>
+					<Button>Sign In</Button>
+				</ButtonsContainer>
+			</HeaderItems>
+		</Container>
+	</React.Fragment>
 );
--- src/layout/components/header/styles.tsx
+++ src/layout/components/header/styles.tsx
@@ -2,7 +2,73 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	position: fixed;
-	height: 74px;
 	background-color: #5cb818;
 	width: 100%;
+	padding: 1rem;
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+`;
+
+export const HeaderItems = styled.div`
+	width: 90%;
+	align-items: center;
+	display: flex;
+	justify-content: space-between;
+	margin: auto;
+`;
+
+export const ButtonsContainer = styled.div`
+	display: flex;
+`;
+
+export const Button = styled.div`
+	padding: 0.25rem 0.5rem;
+	font-size: 1.09375rem;
+	background-color: #fff;
+	color: #5cb818;
+	margin: 0 10px;
+	border-radius: 8px;
+	font-weight: 500;
+	line-height: 1.5;
+
+	:hover {
+		cursor: pointer;
+	}
+`;
+
+export const Logo = styled.div`
+	font-size: 2rem;
+	color: #f2f0f2;
+`;
+
+export const SearchBar = styled.div`
+	width: 60%;
+	position: relative;
+	display: flex;
+
+	input {
+		width: 100%;
+		border: 3px solid transparent;
+		border-right: none;
+		padding: 0.6rem;
+		border-radius: 2.18rem 0 0 2.18rem;
+		outline: none;
+		color: #5cb818;
+		text-overflow: ellipsis;
+		font-size: 1rem;
+	}
+
+	input:focus {
+		color: #5cb818;
+	}
+
+	button {
+		width: 2.5rem;
+		height: 3.125rem;
+		border: 1px solid transparent;
+		text-align: center;
+		color: #fff;
+		border-radius: 0 2.18rem 2.18rem 0;
+		cursor: pointer;
+		font-size: 1.25rem;
+	}
 `;
