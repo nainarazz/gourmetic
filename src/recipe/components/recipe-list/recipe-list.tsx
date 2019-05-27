e6d583685af341705f3f22359590dd22d3fdc95e
Naina Razafindrabiby <nainarazz@gmail.com>
2019-05-27 07:19:33 +0300

    fixed recipe list component to work with pagination

--- src/recipe/components/recipe-list/recipe-list.tsx
+++ src/recipe/components/recipe-list/recipe-list.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import { Container } from './styles';
 import { Recipe } from '../../../graphql-generated-types/query-types';
 import { RecipeCard } from '../recipe-card/recipe-card';
+import { RecipeEdge } from '../../../../api/graphql-generated-types/resolvers-types';
 
 interface RecipeListProps {
-	recipes: Recipe[];
+	recipeEdges: RecipeEdge[];
 }
 
 export const RecipeList: React.SFC<RecipeListProps> = props => {
-	if (!props.recipes) {
+	if (!props.recipeEdges) {
 		return null;
 	}
-	const recipes = props.recipes.map(r => (
-		<RecipeCard
-			key={(r as Recipe)._id}
-			recipe={r as Recipe}
-			totalLikes={15}
-			username={'naina'}
-		/>
-	));
-
+	const recipes = props.recipeEdges.map(e => {
+		const recipe = e.node;
+		return (
+			<RecipeCard
+				key={(recipe as Recipe)._id}
+				recipe={recipe as Recipe}
+				totalLikes={15}
+				username={'naina'}
+			/>
+		);
+	});
 	return <Container>{recipes}</Container>;
 };
--- src/recipe/containers/recipe-root.tsx
+++ src/recipe/containers/recipe-root.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Recipe } from '../../../api/graphql-generated-types/resolvers-types';
+import { RecipeEdge } from '../../../api/graphql-generated-types/resolvers-types';
 import { RecipeList } from '../components/recipe-list/recipe-list';
 import { RecipeListComponent } from '../../graphql-generated-types/query-types';
 
 export const RecipeRoot = () => (
 	<RecipeListComponent>
 		{({ data }) => {
-			const recipes = data && data!.recipeList;
-			return <RecipeList recipes={recipes as Recipe[]} />;
+			const result = data && data.recipeList;
+			const edges = result && result.edges;
+			return <RecipeList recipeEdges={edges as RecipeEdge[]} />;
 		}}
 	</RecipeListComponent>
 );
