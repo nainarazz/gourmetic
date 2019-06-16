a728a48b048cccf45acb4486d3f30b02e55f5d94
Naina Razafindrabiby <nainarazz@gmail.com>
2019-06-16 21:19:48 +0300

    fixed mongodb seed

--- api/migrations/recipe.faker.ts
+++ api/migrations/recipe.faker.ts
@@ -10,16 +10,8 @@ export const generateRecipe = () => ({
 	meal: faker.random.arrayElement(meals),
 	prepTime: faker.random.number({ min: 5, max: 60 }),
 	cookingTime: faker.random.number({ min: 5, max: 60 }),
-	ingredients: {
-		measurement: faker.random.arrayElement(measurements),
-		item: faker.random.word(),
-		quantity: faker.random.number({ min: 1, max: 5 }),
-	},
-	instructions: {
-		imageUrl: '',
-		stepNumber: faker.random.number({ min: 1, max: 5 }),
-		description: faker.lorem.sentence(),
-	},
+	ingredients: createIngredients(faker.random.number({ min: 3, max: 10 })),
+	instructions: createInstructions(faker.random.number({ min: 3, max: 10 })),
 	yield: faker.random.number({ min: 1, max: 5 }),
 	image: '',
 	dietLabels: faker.random.arrayElement(dietLabels),
@@ -28,3 +20,21 @@ export const generateRecipe = () => ({
 	createdAt: new Date(),
 	updatedAt: new Date(),
 });
+
+const createIngredient = () => ({
+	measurement: faker.random.arrayElement(measurements),
+	item: faker.random.word(),
+	quantity: faker.random.number({ min: 1, max: 5 }),
+});
+
+const createIngredients = (numOfIngredients: number) =>
+	Array.from({ length: numOfIngredients }, createIngredient);
+
+const createInstruction = () => ({
+	imageUrl: '',
+	stepNumber: faker.random.number({ min: 1, max: 5 }),
+	description: faker.lorem.sentence(),
+});
+
+const createInstructions = (num: number) =>
+	Array.from({ length: num }, createInstruction);
