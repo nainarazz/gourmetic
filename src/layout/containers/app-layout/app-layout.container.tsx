a4d029023ef5eee17b53db9dcce99d345058932b
Naina Razafindrabiby <nainarazz@gmail.com>
2019-07-15 21:33:17 +0300

    show new recipe btn only when on home page

--- src/layout/containers/app-layout/app-layout.container.tsx
+++ src/layout/containers/app-layout/app-layout.container.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import React, { useState } from 'react';
 import { Backdrop } from '../../../shared/components/backdrop/backdrop.component';
 import { createGlobalStyle } from 'styled-components';
@@ -6,6 +7,7 @@ import { Header } from '../../components/header/header.component';
 import { MainContainer } from '../../components/main/main.component';
 import { PlusButton } from './app-layout.style';
 import { SideDrawer } from '../../components/side-drawer/side-drawer.component';
+import { useRouter } from 'next/router';
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -14,9 +16,20 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
+const urlAtHomePage = (routeUrl: string) => /^\/$/.test(routeUrl);
+
 export const AppLayout: React.SFC = props => {
 	const [sideDrawerIsOpen, setSideDrawerIsOpen] = useState(false);
 	let backdrop: JSX.Element | null = null;
+
+	const userRouter = useRouter();
+	const newRecipeButton = urlAtHomePage(
+		userRouter && userRouter.pathname
+	) && (
+		<Link href={`/new-recipe`} as={`new-recipe`}>
+			<PlusButton>+</PlusButton>
+		</Link>
+	);
 
 	if (sideDrawerIsOpen) {
 		backdrop = <Backdrop click={() => setSideDrawerIsOpen(false)} />;
@@ -36,7 +49,7 @@ export const AppLayout: React.SFC = props => {
 			{backdrop}
 			<SideDrawer isOpen={sideDrawerIsOpen} />
 			<MainContainer>{props.children}</MainContainer>
-			<PlusButton>+</PlusButton>
+			{newRecipeButton}
 		</div>
 	);
 };
---
+++ src/pages/new-recipe.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NextFunctionComponent } from 'next';
+
+const NewRecipePage: NextFunctionComponent = () => {
+	return (
+		<React.Fragment>
+			<div>New Recipe Page here</div>
+		</React.Fragment>
+	);
+};
+
+export default NewRecipePage;
