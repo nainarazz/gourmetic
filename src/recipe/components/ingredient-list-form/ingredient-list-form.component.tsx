c28ac2a23eecb75904a6262afb0cfabba79495a0
Naina Razafindrabiby <nainarazz@gmail.com>
2019-07-30 07:36:23 +0300

    refactored ingredient form

--- src/recipe/components/ingredient-list-form/ingredient-list-form.component.tsx
+++ src/recipe/components/ingredient-list-form/ingredient-list-form.component.tsx
@@ -1,13 +1,9 @@
-import React, { SFC } from 'react';
+import React, { ChangeEvent, SFC, useState } from 'react';
 import { emptyIngredient } from '../../constants/recipe.constants';
 import { FieldArrayRenderProps } from 'formik';
-import { FormValues } from '../../types/recipe.interface';
+import { FormValues, Ingredient } from '../../types/recipe.interface';
+import { GenericInputContainer } from '../../../shared/styles/form';
 import { StyledIngredient } from '../recipe-form/recipe-form.style';
-import {
-	GenericInputContainer,
-	Label,
-	Input,
-} from '../../../shared/styles/form';
 
 interface IngredientListProps {
 	formValues: FormValues;
@@ -17,39 +13,73 @@ interface IngredientListProps {
 export const IngredientListForm: SFC<IngredientListProps> = ({
 	formValues,
 	arrayHelpers,
-}) => (
-	<React.Fragment>
-		{formValues.ingredients.map((ingredient, index) => {
-			return (
-				<StyledIngredient key={index}>
-					<GenericInputContainer>
-						<Label>Item</Label>
-						<Input name={`ingredients[${index}].item`} />
-					</GenericInputContainer>
+}) => {
+	const [ingredient, setIngredient] = useState(emptyIngredient);
 
-					<GenericInputContainer>
-						<Label>Amount</Label>
-						<Input name={`ingredients[${index}].quantity`} />
-					</GenericInputContainer>
+	// tslint:disable-next-line:no-any
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const name = event.target.name;
+		const value = event.target.value;
 
-					<GenericInputContainer>
-						<Label>Measurement</Label>
-						<Input name={`ingredients[${index}].measurement`} />
-					</GenericInputContainer>
-					<button
-						type="button"
-						onClick={() => arrayHelpers.remove(index)}
-					>
-						Remove Ingredient
-					</button>
-				</StyledIngredient>
-			);
-		})}
-		<button
-			type="button"
-			onClick={() => arrayHelpers.push(emptyIngredient)}
-		>
-			Add Ingredient
-		</button>
-	</React.Fragment>
-);
+		const modifiedIngredient: Ingredient = {
+			...ingredient,
+			[name]: value,
+		};
+		setIngredient(modifiedIngredient);
+	};
+
+	return (
+		<React.Fragment>
+			{formValues.ingredients.map((ing, index) => {
+				return (
+					<div key={index}>
+						{`${ing.quantity} ${ing.measurement} ${ing.item}`}
+						<button
+							type="button"
+							onClick={() => arrayHelpers.remove(index)}
+						>
+							Remove Ingredient
+						</button>
+					</div>
+				);
+			})}
+			<StyledIngredient>
+				<GenericInputContainer>
+					<label>Quantity</label>
+					<input
+						name="quantity"
+						value={ingredient.quantity}
+						onChange={handleChange}
+					/>
+				</GenericInputContainer>
+
+				<GenericInputContainer>
+					<label>Measurement</label>
+					<input
+						name="measurement"
+						value={ingredient.measurement}
+						onChange={handleChange}
+					/>
+				</GenericInputContainer>
+
+				<GenericInputContainer>
+					<label>Item</label>
+					<input
+						value={ingredient.item}
+						name="item"
+						onChange={handleChange}
+					/>
+				</GenericInputContainer>
+			</StyledIngredient>
+			<button
+				type="button"
+				onClick={() => {
+					arrayHelpers.push(ingredient);
+					setIngredient(emptyIngredient);
+				}}
+			>
+				Add Ingredient
+			</button>
+		</React.Fragment>
+	);
+};
